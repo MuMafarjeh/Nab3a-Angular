6a4ca5db0c87ab1b55c5cba49d9a31d9f052d16a
Abed Alamleh <34231700+abed1996@users.noreply.github.com>
2019-04-14 14:06:06 +0300

    filtering on order

--- src/app/business-order/business-order-component/order.ts
+++ src/app/business-order/business-order-component/order.ts
@@ -1,13 +1,13 @@
 import { DateAdapter } from '@angular/material';
+import { Timestamp } from 'rxjs';
+import { Time } from '@angular/common';
 export interface Order
 {
-
     name: string;
     price: number;
     timeGenerated: Date;
     timeReceiving: Date;
     id:string;
-
-
-
+    TGDate: Date;
+    TRDate: Date;
 }
--- src/app/business-order/business-order.component.ts
+++ src/app/business-order/business-order.component.ts
@@ -1,7 +1,8 @@
 import { Order } from './business-order-component/order';
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
 import { OrderServiceService } from './business-order-component/order-service.service';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-business-order',
@@ -10,13 +11,36 @@ import { OrderServiceService } from './business-order-component/order-service.se
 })
 export class BusinessOrderComponent implements OnInit {
 
-    date = new FormControl(new Date());
-    serializedDate = new FormControl((new Date()).toISOString());
+  formDate: FormGroup = new FormGroup({
+    fromDate: new FormControl(''),
+    toDate: new FormControl(''),
+  });
+
+  date = new FormControl(new Date());
+  serializedDate = new FormControl((new Date()).toISOString());
 
   constructor(private orderService: OrderServiceService) { }
- orders :Order[];
+  orders: Order[];
+
+  copyOrders: Order[];
+
+  ordersFilter: Order[] = [];
+
   ngOnInit() {
     this.orders = this.orderService.getOrder();
+    this.copyOrders = this.orders;
+  }
+  filter() {
+    this.ordersFilter = [];
+    this.copyOrders.forEach(element => {
+      var fromDate = element.TGDate;
+      var toDate = element.TRDate;
+      if ((fromDate.getTime() >= this.formDate.controls['fromDate'].value.getTime()) && (fromDate.getTime() <= this.formDate.controls['toDate'].value.getTime())) {
+        this.ordersFilter.push(element);
+      }
+    });
+
+    this.orders = this.ordersFilter;
   }
 
 }
