fb3401b1c5f328dc1785e519da80483e982cae1c
RamiYahyaRY <rami.yahya.yes@gmail.com>
2019-06-21 16:17:05 +0300

    Firebase hosting config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,18 @@
 export const environment = {
   production: true,
-  hmr: false
+  hmr: false,
+  
+  firebase: {
+    apiKey: "AIzaSyBJKVogWGYun4CYiNnFD4yN7i1NnCQ7uEg",
+    authDomain: "nab3a-com.firebaseapp.com",
+    databaseURL: "https://nab3a-com.firebaseio.com",
+    projectId: "nab3a-com",
+    storageBucket: "nab3a-com.appspot.com",
+    messagingSenderId: "971682683190"
+  },
+
+  algolia: {
+    appId: '2F1XIQFXH1',
+    apiKey: '5c15a1e7fb5c0172d05af1f23fae6686' //search-only
+  }
 };
